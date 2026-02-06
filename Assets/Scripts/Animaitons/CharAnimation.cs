a03145541bbc9c02317a2737ec38c2a588d5c362
ccalpiz <ccalpiz.sato@gmail.com>
2026-02-06 23:29:03 +0700

    Character Prefabs

---
+++ Assets/Scripts/Animaitons/CharAnimation.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CharAnimation : MonoBehaviour
+{
+    private Character character;
+
+    void Start()
+    {
+        character = GetComponent<Character>();
+    }
+
+    private void ChooseAnimation(Character c)
+    {
+        c.Anim.SetBool("IsIdle", false);
+        c.Anim.SetBool("IsWalk", false);
+
+        switch (c.State)
+        {
+            case CharState.Idle:
+                c.Anim.SetBool("IsIdle", true);
+                break;
+            case CharState.Walk:
+                c.Anim.SetBool("IsWalk", true);
+                break;
+        }
+    }
+
+    void Update()
+    {
+        ChooseAnimation(character);
+    }
+}
